21fd7da5b8d70c5a80bef318f9158be980fb9546
lyndabelfar <lyndabelar@gmail.com>
2022-08-19 00:39:27 +0100

    second commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,17 +4,31 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 function Navbar() {
+    const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+    const toggleLinks = () => {
+        setShowLinks(!showLinks);
+    };
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if (showLinks) {
+        linksContainerRef.current.style.height = `${linksHeight}px`;
+        } else {
+        linksContainerRef.current.style.height = '0px';
+        }
+    }, [showLinks]);
   return (
     <nav>
         <div className="nav-center">
             <div className="nav-header">
                 <img src={logo} alt="" />
-                <button className="nav-toggle">
+                <button className="nav-toggle" onClick={toggleLinks}>
                     <FaBars /> 
                 </button>
             </div>
-            <div className="links-container show-container">
-                <ul className="links">
+            <div className="links-container" ref={linksContainerRef}>
+                <ul className="links" ref={linksRef}>
                     {links.map((link)=>{
                         const {url, id , text} = link;
                         return <li key={id}>
